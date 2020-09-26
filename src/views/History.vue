40c8ff7c9e618e0eaf032020c4711297e78d6fb2
Diana Mamedova <51512169+strhrt@users.noreply.github.com>
2020-09-26 14:49:01 +0300

    add chart js in history page

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
     <Loader v-if="loading" />
     <p class="center" v-else-if="!records.length">
@@ -30,12 +30,14 @@
 <script>
 import paginationMixin from "@/mixins/pagination.mixin";
 import HistoryTable from "@/components/HistoryTable";
+import { Pie } from "vue-chartjs";
 
 export default {
   name: "History",
   components: {
     HistoryTable,
   },
+  extends: Pie,
   mixins: [paginationMixin],
   data: () => ({
     loading: true,
@@ -44,18 +46,57 @@ export default {
   async mounted() {
     this.records = await this.$store.dispatch("fetchRecords");
     const categories = await this.$store.dispatch("fetchCategories");
-    this.setupPagination(
-      this.records.map((record) => {
-        return {
-          ...record,
-          categoryName: categories.find((c) => c.id === record.categoryId)
-            .title,
-          typeClass: record.type === "income" ? "green" : "red",
-          typeText: record.type === "income" ? "доход" : "расход",
-        };
-      })
-    );
+
+    this.setup(categories);
+
     this.loading = false;
+  },
+  methods: {
+    setup(categories) {
+      this.setupPagination(
+        this.records.map((record) => {
+          return {
+            ...record,
+            categoryName: categories.find((c) => c.id === record.categoryId)
+              .title,
+            typeClass: record.type === "income" ? "green" : "red",
+            typeText: record.type === "income" ? "доход" : "расход",
+          };
+        })
+      );
+      this.renderChart({
+        labels: categories.map((c) => c.title),
+        datasets: [
+          {
+            label: "Расходы по категориям",
+            data: categories.map((c) => {
+              return this.records.reduce((acc, rec) => {
+                if (rec.categoryId === c.id && rec.type === "outcome")
+                  acc += +rec.amount;
+                return acc;
+              }, 0);
+            }),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      });
+    },
   },
 };
 </script>
