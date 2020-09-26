2caa650b0720e6f139bfba1f3e1b2b0ae66a9e8c
Diana Mamedova <51512169+strhrt@users.noreply.github.com>
2020-09-26 15:37:42 +0300

    add localize filter

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="sidenav app-sidenav" :class="{open: value}">
+  <ul class="sidenav app-sidenav" :class="{ open: value }">
     <router-link
       v-for="link in links"
       :key="link.url"
@@ -8,36 +8,37 @@
       :to="link.url"
       :exact="link.exact"
     >
-      <a class="waves-effect waves-orange pointer">{{link.title}}</a>
+      <a class="waves-effect waves-orange pointer">{{ link.title }}</a>
     </router-link>
   </ul>
 </template>
 
 <script>
+import localizeFilter from "@/filters/localize.filter";
 export default {
   name: "Sidebar",
   props: ["value"],
   data: () => ({
     links: [
       {
-        title: "Счет",
+        title: localizeFilter("Menu_Bill"),
         url: "/",
         exact: true,
       },
       {
-        title: "История",
+        title: localizeFilter("Menu_History"),
         url: "/history",
       },
       {
-        title: "Планирование",
+        title: localizeFilter("Menu_Planning"),
         url: "/planning",
       },
       {
-        title: "Новая запись",
+        title: localizeFilter("Menu_NewRecord"),
         url: "/record",
       },
       {
-        title: "Категории",
+        title: localizeFilter("Menu_Categories"),
         url: "/categories",
       },
     ],
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
     <div class="page-title">
-      <h3>Профиль</h3>
+      <h3>{{ "ProfileTitle" | localize }}</h3>
     </div>
-
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
-        <input id="description" type="text" />
-        <label for="description">Имя</label>
-        <span class="helper-text invalid">name</span>
+        <input
+          id="description"
+          type="text"
+          v-model="name"
+          :class="{
+            invalid: $v.name.$dirty && !$v.name.required,
+          }"
+        />
+        <label for="description">{{ "Name" | localize }} </label>
+        <small
+          v-if="$v.name.$dirty && !$v.name.required"
+          class="helper-text invalid"
+          >{{ "Message_EnterName" | localize }}
+        </small>
       </div>
-
+      <div class="switch" style="margin-bottom: 20px">
+        <label style="font-size: 1rem">
+          English
+          <input type="checkbox" v-model="isRuLocale" />
+          <span class="lever"></span>
+          Русский
+        </label>
+      </div>
       <button class="btn waves-effect waves-light" type="submit">
-        Обновить
+        {{ "Update" | localize }}
         <i class="material-icons right">send</i>
       </button>
     </form>
@@ -20,10 +37,45 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+import { required } from "vuelidate/lib/validators";
+
 export default {
   name: "Profile",
+  data: () => ({
+    name: "",
+    isRuLocale: true,
+  }),
+  validations: {
+    name: {
+      required,
+    },
+  },
+  computed: mapGetters(["info"]),
+  mounted() {
+    this.name = this.info.name;
+    this.isRuLocale = this.info.locale === "ru-RU";
+    setTimeout(() => {
+      M.updateTextFields();
+    });
+  },
+  methods: {
+    ...mapActions(["updateInfo"]),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name,
+          locale: this.isRuLocale ? "ru-RU" : "en-US",
+        });
+      } catch (err) {}
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
